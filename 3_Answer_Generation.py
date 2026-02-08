6d811d4b4b6cd06749567116677e7af7e879fa1a
Jitesh Pahwa <jiteshofficial2004@gmail.com>
2026-02-08 18:57:19 +0530

    Added answer generation using opn ai gpt-4o

---
+++ 3_Answer_Generation.py
@@ -0,0 +1,77 @@
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
+
+load_dotenv()
+
+persist_directory = "db/chroma_db"
+
+embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+
+db = Chroma(
+    persist_directory=persist_directory,
+    embedding_function=embedding_model,
+    collection_metadata={"hnsw:space": "cosine"},
+)
+
+# Search for relevant documents
+query = "Which island does SpaceX lease for its launches in the Pacific?"
+
+retriever = db.as_retriever(search_kwargs={"k": 5})
+
+# retriever = db.as_retriever(
+#     search_type="similarity-score-threshold",
+#     search_kwargs={
+#         "k": 5,
+#         "score_threshold": 0.3,  # Only return chunks with cosine similarity above 0.3
+#     },
+# )
+
+relevant_docs = retriever.invoke(query)
+
+print(f"\nUser Query: {query}")
+# Display
+print("--- Context ---")
+for i, doc in enumerate(relevant_docs):
+    print(f"\nDocument {i+1}:")
+    print(f"  Source: {doc.metadata['source']}")
+    print(f"  Content length: {len(doc.page_content)} characters")
+    print(f"  Content preview: {doc.page_content[:200]}...")
+
+# chr(10) is newline character
+combined_input = f"""Based on the following documents, please answer this question: {query}
+
+Documents:
+{chr(10).join([f"- {doc.page_content}" for doc in relevant_docs])} 
+
+Please provide a clear and helpful answer using only the information from these documents. If you can't find the answer in the documents, say "I don't have enough information to answer that question based on the provided documents."."""
+
+model = ChatOpenAI(model="gpt-4o")
+
+messages = [
+    SystemMessage(
+        content="You are a helpful assistant that provides answers based on the provided context."
+    ),
+    HumanMessage(content=combined_input),
+]
+
+resutl = model.invoke(messages)
+
+print("\n--- Generated Answer ---")
+print("Full result:")
+print(resutl)
+print("\nContent only:")
+print(resutl.content)
+
+# Synthetic Questions:
+
+# 1. "What was NVIDIA's first graphics accelerator called?"
+# 2. "Which company did NVIDIA acquire to enter the mobile processor market?"
+# 3. "What was Microsoft's first hardware product release?"
+# 4. "How much did Microsoft pay to acquire GitHub?"
+# 5. "In what year did Tesla begin production of the Roadster?"
+# 6. "Who succeeded Ze'ev Drori as CEO in October 2008?"
+# 7. "What was the name of the autonomous spaceport drone ship that achieved the first successful sea landing?"
+# 8. "What was the original name of Microsoft before it became Microsoft?"
